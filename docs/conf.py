3742251c4931f227341ad7a952f379d9b20ebf99
Amaya William <amayaw376@gmail.com>
2025-06-18 12:18:20 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,5 +9,4 @@ exclude_patterns = []
 html_theme = 'alabaster'
 html_static_path = ['_static']   
 
-<meta name="google-site-verification" content="PkOcJ7MGTo64kIFUiueUfkoZR7zBlaDUXcYYdm2aZoc" />
-<meta name="msvalidate.01" content="78CF600A919012E8E5E8E326F92AF322" />
+
